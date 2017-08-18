a3d5cfa754860792d478ab04612210202e636a67
Go Suzuki <j1618@matsue-ct.jp>
2017-08-18 11:11:16 +0900

    Created the project.

---
+++ Procon2017MCTProtocol/Procon2017MCTProtocol/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Procon2017MCTProtocol
+{
+    [ServiceContract]
+    public interface IProconPuzzleService {
+        [OperationContract]
+        SendablePolygon Polygon();
+        [OperationContract]
+        string QRCode();
+    }
+
+    public struct SendablePoint {
+        public int X;
+        public int Y;
+
+        public SendablePoint(int X, int Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+
+    }
+
+    public class SendablePolygon {
+        public List<SendablePoint> Points { get; set; }
+    }
+}
